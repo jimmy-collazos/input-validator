e4facc513cfb7d29257b89b030dc2f058a6fe8be
Jimmy Collazos <acido69@gmail.com>
2013-12-07 00:57:03 +0100

    fixed errors when return value to scope

--- src/01_directive.js
+++ src/01_directive.js
@@ -1,19 +1,24 @@
 angular.module('input-validator').directive('validator', function($validator) {
-    var controller; //, validators;
+    var ERROR_REGEXP  = /0/;
+    var controller;
     var nexvalidate = function(arrValidators){
         arrValidators = angular.isArray(arrValidators) ? arrValidators : [arrValidators];
         return function(value){
             var currentValue = angular.isDefined(value) ? value : '';
-            var errors = $validator.validate(value, arrValidators);
+            var errors = $validator.validate(currentValue, arrValidators);
+            var isEmpty = controller.$isEmpty(value);
+            var result = '';
             angular.forEach(errors, function(resultOfTest, key){
-                var isValid = controller.$isEmpty(currentValue) || resultOfTest;
+                var isValid = isEmpty || resultOfTest;
+                result += isValid ? '1' : '0';
                 controller.$setValidity(key, isValid);
             });
+            return ERROR_REGEXP.test(result) ? undefined : value;
         }
     };
 
     return {
-        require: "ngModel",
+        require: 'ngModel',
         scope: { validator: '=validator' },
         link: function(scope, elm, attrs, ctrl) {
             validators = scope.validator;
