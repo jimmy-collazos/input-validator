d05c26e6050469906fcee405621b1e0f7b1c2d8b
Jimmy Collazos <acido69@gmail.com>
2013-12-09 23:56:06 +0100

    configure build file

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,16 +1,51 @@
 module.exports = function(grunt) {
+    grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-replace');
     grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        concat: {
+            options: {
+                separator: ';'
+            },
+            dist: {
+                src: ['src/**/*.js'],
+                dest: 'dist/input-validator.js'
+            },
+        },
+        replace: {
+            dist:{
+                options: {
+                    patterns: [
+                        {
+                            match: '/;+angular\.module\\(\'input-validator\'\\)/g',
+                            replacement: '',
+                            expression: true
+                        }
+                    ]
+                },
+                files: [
+                    {
+                        src: ['dist/input-validator.js'],
+                        dest: 'dist/input-validator.js'
+                    }
+                ]
+            }
+        },
         uglify: {
-            my_target: {
-              options: {
-                sourceMapRoot: 'src', // the location to find your original source
-              },
-              files: {
-                'dist/input-validator.min.js': ['dist/input-validator.js'],
-              }
+            dist: {
+                options: {
+                    banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
+                        '<%= grunt.template.today("yyyy-mm-dd") %> */\r\n',
+                    mangle: {
+                        except: ['angular']
+                    }
+                },
+                files: {
+                    'dist/input-validator.min.js': ['dist/input-validator.js'],
+                }
             }
         }
     });
-    grunt.registerTask('build', ['uglify']);
+    grunt.registerTask('build', ['concat', 'replace', 'uglify']);
 }
--- src/01_provider.js
+++ src/01_provider.js
@@ -1,13 +1,11 @@
 angular.module('input-validator').provider('$validator', function() {
+    var AUTO_ID_PREFIX = 'validator';
     var validators = this.$$validators = {};
-
     this.$get = function(){
-
         return {
             register: function(id, fnTest){
                 if(validators.hasOwnProperty(id)) throw new Error('Validator ID is already exist');
                 if( ! angular.isFunction(fnTest) ) throw new Error('Validator test funcion not is defined!');
-
                 validators[id] = fnTest;
             },
             get:function(id){
@@ -17,27 +15,22 @@ angular.module('input-validator').provider('$validator', function() {
                 var result = {};
                 angular.forEach(arrValidatorIdOrFnTest, function(idOrFunction, index){
                     var id, fnTest;
-
                     if( angular.isString(idOrFunction) ){
                         id = idOrFunction;
                         fnTest = validators[id];
                     } else if( angular.isFunction(idOrFunction) ){
-                        id = 'validator'+index;
+                        id = AUTO_ID_PREFIX+index;
                         fnTest = idOrFunction;
                     }else if( angular.isObject(idOrFunction) && idOrFunction.id && angular.isFunction( idOrFunction.test) ){
                         id = idOrFunction.id;
                         fnTest = idOrFunction.test;
                     }
-
                     if(id){
                         this[id] = fnTest(value);
                     }
-
                 }, result);
-
                 return result;
             }
         }
     };
-
 });
